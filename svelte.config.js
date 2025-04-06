aa0d9ae27e44b67b1d457542dfb139e4825eb8ba
nitromelon <github.z6b5z@simplelogin.com>
2025-04-06 20:34:06 +0700

    On branch duyanh Changes to be committed: 	new file:   .github/workflows/deploy.yml 	modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,9 @@ const config = {
 
     kit: {
         adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === "production" ? process.env.BASE_PATH : "",
+        },
     },
 };
 
